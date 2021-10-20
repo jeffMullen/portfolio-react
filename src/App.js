9a8e6f4956b7f07ec6aca83e9c5937c377d0ce48
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-10-20 12:20:11 -0400

    condintional rendering set up with nav-link glow when active

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
@@ -6,7 +7,6 @@ import Applications from './components/Applications';
 import Footer from './components/Footer';
 
 function App() {
-
   function modalHandler(title, deployed, github, tech, description) {
     let modalTitle = document.querySelector('.modal-title')
     let liveApp = document.querySelector('#liveApp');
@@ -21,13 +21,30 @@ function App() {
     projectDescription.innerHTML = description;
   }
 
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Skills') {
+      return <Skills />;
+    }
+    if (currentPage === 'Applications') {
+      return <Applications modalHandler={modalHandler} />;
+    }
+    // if (currentPage === 'Contact')
+    // return <Contact />;
+  };
+
+  const pageChange = (page) => setCurrentPage(page);
+
+
   return (
     <div id="reactApp">
-      <Header />
+      <Header currentPage={currentPage} pageChange={pageChange} />
       <main>
-        <About />
-        <Skills />
-        <Applications modalHandler={modalHandler} />
+        {renderPage()}
       </main>
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function Header() {
+function Header({ currentPage, pageChange }) {
     return (
         <header>
             <h1>Jeff Mullen</h1>
             <nav>
                 <ul>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#applications">Applications</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#about" onClick={() => pageChange('About')} className={currentPage === 'About' ? 'active' : ''}>About</a></li>
+                    <li><a href="#skills" onClick={() => pageChange('Skills')} className={currentPage === 'Skills' ? 'active' : ''}>Skills</a></li>
+                    <li><a href="#applications" onClick={() => pageChange('Applications')} className={currentPage === 'Applications' ? 'active' : ''}>Applications</a></li>
+                    <li><a href="#contact" onClick={() => pageChange('Contact')} className={currentPage === 'Contact' ? 'active' : ''}>Contact</a></li>
                 </ul>
             </nav>
         </header>
