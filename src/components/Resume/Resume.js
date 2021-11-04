8f0a9b1c426ab42391d117b8f401faeaafc699cb
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-04 14:39:47 -0400

    updating resume and deleting old resumes

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Skills from './Skills';
 
-import resumePDF from '../../images/jeffrey-mullen-resume.pdf';
+import resumePDF from '../../images/jeff-mullen.pdf';
 
 function Resume() {
     return (
