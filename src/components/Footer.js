822f3ca9ec17c69d8f9e316f169b0a89b4de78d9
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-11-18 16:01:32 -0500

    deleting twitter icon and link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import gitHubImg from '../images/github-logo-2.png';
 import linkedInImg from '../images/linkedin.svg';
-import twitterImg from '../images/twitter.svg';
+// import twitterImg from '../images/twitter.svg';
 
 function Footer() {
     return (
@@ -10,7 +10,6 @@ function Footer() {
 
             <a className="px-3" href="https://github.com/jeffMullen" rel="noreferrer" target="_blank"><img src={gitHubImg} width="40" alt="github link"></img></a>
             <a className="px-3" href="https://linkedin.com/in/jeff-mullen27" rel="noreferrer" target="_blank"><img src={linkedInImg} width="40" alt="linked in link"></img></a>
-            <a className="px-3" href="https://twitter.com/JeffMul70036269" rel="noreferrer" target="_blank"><img src={twitterImg} width="40" alt="twitter link"></img></a>
         </footer>
     )
 }
