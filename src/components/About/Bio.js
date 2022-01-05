6b32148a96290cb8316e469afb90dd5863c7718c
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-01-05 15:57:08 -0500

    updating bio

--- src/components/About/Bio.js
+++ src/components/About/Bio.js
@@ -21,24 +21,16 @@ function Bio() {
                     <img className="p-0" src={selfPortrait} alt="Self-Portrait" />
                 </Col>
                 <Col className="about-paragraph mt-5 mt-lg-0 px-0 col-md-8 col-lg-7 col-xl-4 order-lg-1 align-self-xl-start">
-                    <p className="p-3 mb-0">Hi. My name is Jeff. I am a developer who strives for truly presentable applications. There is a
-                        common thread throughout my life, which is that I have a methodology to the things I do. As a
-                        Massage Therapist I was able to provide excellent work for my clients as a result of setting
-                        myself up
-                        correctly. I studied the inner workings of the body to a level of understanding that informed
-                        the quality of work I gave. I set myself up with a framework that narrowed my focus down to
-                        anatomical structures of the body, and the assessment of what those structures needed
-                        therapeutically. Using this methodology ultimately allowed for my creativity and intuition to
-                        shine. This work ethic is translated into my baking, gardening, music, and coding.
+                    <p className="p-3 mb-0">Hi. My name is Jeff. I am a developer who strives to create truly presentable applications. A common thread throughout my life is that I have a methodology to the things I do. As a Massage Therapist, I studied the inner workings of the body to a level of understanding that informed the quality of work I gave. I used this framework to narrow my focus to anatomical structures of the body, and assess what those structures needed therapeutically. Using this methodology ultimately allowed for my creativity and intuition to shine. This work ethic is translated into my coding, baking, and music.
                     </p>
                 </Col>
                 <Col className="about-paragraph mt-5 p-0 col-md-8 col-lg-7 col-xl-4 text-center order-lg-3 align-self-xl-end">
                     <p className="p-3 m-0">This methodology is what allows me to thrive in the development of quality applications.
                         Functionality and presentation are linked together. Studying and internalizing the fundamentals
                         of programming allow for balance within my process. Through my experiences I have developed the
-                        ability to empathize with people, and discover what their needs are. This is a skill that I find
+                        ability to empathize with others, and discover what their needs are. This is a skill that I find
                         valuable in the creation of web applications, as the end-users of the product will have needs,
-                        such as accesibility and ease of use, that need met.</p>
+                        such as accesibility and ease of use, that require thoughtful action.</p>
                 </Col>
             </Row>
         </Container>
