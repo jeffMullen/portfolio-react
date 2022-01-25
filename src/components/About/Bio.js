240279c8390207f8c2fdb6fb1ad4b3d26637ca4c
Walrus <81767572+jeffMullen@users.noreply.github.com>
2022-01-25 13:33:14 -0500

    updating bio photo

--- src/components/About/Bio.js
+++ src/components/About/Bio.js
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import selfPortrait from '../../images/Jeff-Mullen-Headshot.png';
+import selfPortrait from '../../images/Jeff-Mullen-LinkedIn-narrow.jpeg';
 
 function Bio() {
     return (
